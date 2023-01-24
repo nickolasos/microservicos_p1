d194e09dc025486b1f9346aecee7bf3bd40fa0df
Nickolas Oliveira Soares <nickolas.osoares@gmail.com>
2023-01-23 22:40:04 -0300

    cria dockerfile, ajusta docker compose

--- auth-api/app.js
+++ auth-api/app.js
@@ -4,7 +4,7 @@ const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 
-app.get('/api-status',(req,res)=>{
+app.get('/api/status',(req,res)=>{
     return res.status(200).json({
         service : 'Auth-API',
         status: 'up',
